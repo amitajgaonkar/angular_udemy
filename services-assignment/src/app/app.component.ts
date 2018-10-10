614281f5d8e6538bcc871c57a1a06298c7fc85b7
Amit Ajgaonkar <Amit.Ajgaonkar@extentia.com>
2018-10-10 18:47:06 +0530

    Latest changes

--- services-assignment/src/app/app.component.ts
+++ services-assignment/src/app/app.component.ts
@@ -1,21 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { UsersService } from './users.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
+  private activeUsers;
+  private inactiveUsers;
+  constructor(private userService: UsersService ) {}
 
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
+  ngOnInit(){
+    this.activeUsers = this.userService.activeUsers;
+    this.inactiveUsers = this.userService.inactiveUsers;
   }
 
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
-  }
 }
