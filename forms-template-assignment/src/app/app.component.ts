87968c2575240b694af4bc0283e278d5a0adaf31
Amit Ajgaonkar <Amit.Ajgaonkar@extentia.com>
2018-12-11 16:09:03 +0530

    Template form assignment completed

--- forms-template-assignment/src/app/app.component.ts
+++ forms-template-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  @ViewChild('f') regForm: NgForm
+  
+  subsriptionsList = ['Basic', 'Advanced', 'Professional']
+  submittedValues;
+  defaultSubsription = 'Advanced';
+  
+  onSubmit(){
+    console.log(this.regForm);
+  }
 }
